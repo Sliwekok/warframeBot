4b6a0c81946f13c49ce69e8f11379ab906cb56f5
Sliwekok <dominiksliwek@gmail.com>
2023-09-03 22:19:11 +0200

    added possibility to add items to watch list, added strict types

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,11 @@
 // import scss
 import './styles/app.scss';
 
-// create global $ and jQuery variables
-const $ = require('jquery');
-global.$ = global.jQuery = $;
+// enable bootstrap tooltips
+$('[data-toggle="tooltip"]').tooltip();
 
 require('./main.js');
 require('./watched.js');
 require('./bootstrap.js');
 require('./account.js');
 require('./addNew');
-require('./itemFetcher');
-require('./tooltips');
