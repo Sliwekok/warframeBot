ed4f45dc5028092c2ce25586d6b0311b1c32e005
sliwekok <dominiksliwek@gmail.com>
2023-11-01 17:37:56 +0100

    minor fixes

--- _external/sql/view/items_on_watchlist.sql
+++
@@ -1,8 +0,0 @@
-/*
- * view shows all all items with platform name
- * used in ItemController -> watched
- */
-create or alter view items_on_watchlist as
-select i.id, i.login_id, i.name, i.price, i.wiki_url, i.image_url, p.name as platform_name
-from item i
-         inner join platform p on i.platform_id = p.id
--- src/Repository/ItemRepository.php
+++ src/Repository/ItemRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use App\Entity\Item;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Query\Parameter;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
